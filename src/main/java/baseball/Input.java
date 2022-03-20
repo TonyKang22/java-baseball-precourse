03e000342de449a7f14c93cabc7f9db768df8e97
Tony Kang <tonykang@Tonys-MacBook-Pro.local>
2022-03-20 18:06:40 +0900

    [002-RESPONSE] add Exception

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -8,12 +8,17 @@ import java.util.List;
 public class Input {
 
     private final int AGAIN = 1;
-    
+    private final int TERMINATE = 2;
+
     public Input() {
     }
 
     public List<Integer> inputPlayerAnswer() {
         String playerAnswer = Console.readLine();
+
+        if (playerAnswer.length() != 3) {
+            throw new IllegalArgumentException();
+        }
 
         List<Integer> playerAnswerList = new ArrayList<>();
         for (int i = 0; i < 3; ++i) {
@@ -24,8 +29,10 @@ public class Input {
     }
 
     public boolean anotherRound() {
-        String num = Console.readLine();
-        int playerResponse = num.charAt(0) - '0';
+        int playerResponse = Console.readLine().charAt(0) - '0';
+        if (playerResponse != AGAIN && playerResponse != TERMINATE) {
+            throw new IllegalArgumentException();
+        }
         return playerResponse == AGAIN;
     }
 }
